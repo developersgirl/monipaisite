adc33392379ac5f5928d1cf0082a06b7421c814d
developersgirl <xusninigor17@gmail.com>
2023-06-09 08:48:41 +0500

    Initial commit

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import  './navbar.css'
+
+
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <h1>hello world</h1>
+        </div>
+     );
+}
+ 
+export default Navbar;
