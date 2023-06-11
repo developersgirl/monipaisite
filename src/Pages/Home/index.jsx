def391e3a947a2d164837519832199015375bf23
developersgirl <xusninigor17@gmail.com>
2023-06-11 23:01:55 +0500

    Add start website

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,62 @@
-const Home = () =>{
+import React from 'react';
+import './index.css'
+import logo from '../img/logo (1).png';
+import shopping from '../img/shopping-cart (3) 1.png';
+import map from '../img/map.png';
+import yellow from '../img/yellow.png';
+import farro from '../img/farro 1.png';
+import arrow from '../img/right-arrow (1) 1.png';
+
+
+const Home = () => {
     return (
         <div className="home">
-            home
+            <div className="head">
+                <div className="navbar">
+                    <div className="logo">
+                        <img src={logo} />
+                    </div>
+                    <div className="groupmenu">
+                        <a href="">Home</a>
+                        <a href="">Products</a>
+                        <a href="">My Lists</a>
+                        <a href="">Sales & Discounts</a>
+                    </div>
+                    <div className="rightmenu">
+                        <a href="#"> <img src={shopping} /> 2 item</a>
+                        <a href="#">Sign In</a>
+                    </div>
+                </div>
+                <div className="center">
+                    <h1>YOU CAN SAVE  UP TO 50$ <br />
+                        ON AVERAGE SMART GROCERY SHOPPING</h1>
+                    <p>Plan your shopping, find cheapest grocery prices <br />
+                        and save more. </p>
+                    <div className="box">
+                        <div className="first">
+                            <p>City</p>
+                            <h4>Hamilton</h4>
+                        </div>
+                        <div className="first">
+                            <p>Region</p>
+                            <h4>Whitiora</h4>
+                        </div>
+                        <div className="line"></div>
+                        <img src={map} />
+                        <div className="first rasm">
+                            <p>Store</p>
+                            <img src={yellow} />
+                            <img src={farro} />
+                        </div>
+                        <div className="btn">
+                            <p>Let's assemble the cart</p>
+                            <img src={arrow} />
+                        </div>
+
+                    </div>
+                    b ghcfgxtrfyubkhu
+                </div>
+            </div>
         </div>
     )
 
